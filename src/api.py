5064e1d8d7abd7a8c7ff344ba6598c434f43069d
Baltasar12 <baltasarbarbaglia@gmail.com>
2026-06-13 12:52:27 -0300

    added cors compatibility

--- src/api.py
+++ src/api.py
@@ -96,7 +96,11 @@ app = FastAPI(
 # Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://worldcup-predict-pearl.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
